e60365bd7a725eb66befda6bd4b0fb572a63b41b
jodusnodus <billiet.thomas@outlook.com>
2017-10-31 18:05:22 +0100

    Add GameCanvas

---
+++ breakout-backend/src/java/nu/smashit/core/GameCanvas.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nu.smashit.core;
+
+/**
+ *
+ * @author jodus
+ */
+public class GameCanvas {
+
+    public static int HEIGHT = 450;
+    public static int WIDTH = 300;
+}
