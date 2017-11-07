d8d94b4e4dcc98c3393990fa150e8565944b8adb
AnseelJonas <jonas.anseel@student.howest.be>
2017-11-07 20:12:36 +0100

    Add Brick and BrickType in java

---
+++ breakout-backend/src/java/nu/smashit/core/Brick.java
@@ -0,0 +1,32 @@
+package nu.smashit.core;
+
+// @author Jonas
+public class Brick extends Body{
+
+    private BrickType type;
+    private int remainingSmashesNeeded;
+    
+    public Brick(int x, int y, int width, int height, BrickType type) {
+        super(x, y, width, height);
+        this.type = type;
+        this.remainingSmashesNeeded = type.getBrickStrength();
+    }
+
+    public BrickType getType() {
+        return type;
+    }
+
+    public int getRemainingSmashesNeeded() {
+        return remainingSmashesNeeded;
+    }
+    
+    public boolean smashBrick(){
+        remainingSmashesNeeded --;
+        return isBroken();       
+    }
+    
+    public boolean isBroken(){
+        return remainingSmashesNeeded < 1;
+    }
+    
+}
---
+++ breakout-backend/src/java/nu/smashit/core/BrickType.java
@@ -0,0 +1,48 @@
+package nu.smashit.core;
+
+// @author Jonas
+public class BrickType {
+
+    public enum BrickSort{
+        NORMAL,
+        POWERUP,
+        POWERDOWN
+    }
+    
+    private String name;
+    private BrickSort sort;
+    private int brickStrength;
+    private int points;
+    //TODO extra fields for data powerups and powerdowns
+
+    private static final int DEFAULT_BRICK_STRENGTH = 1;
+    private static final int DEFAULT_POINTS = 10;
+    
+    public BrickType(String name, BrickSort sort){
+        this(name, sort, DEFAULT_BRICK_STRENGTH, DEFAULT_POINTS);
+    }
+    
+    public BrickType(String name, BrickSort sort, int brickStrength, int points) {
+        this.name = name;
+        this.sort = sort;
+        this.brickStrength = brickStrength;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BrickSort getSort() {
+        return sort;
+    }
+
+    public int getBrickStrength() {
+        return brickStrength;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+     
+}
