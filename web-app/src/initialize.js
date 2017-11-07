eff10e38fc8008a0a4fe3ddff02bec4154dfcec6
svend <sven.depickere@student.howest.be>
2017-11-07 17:02:30 +0100

    Update CSS / HTML
    Created a home UI and setup for singleplayer. Created Screenheight js and playmusic js files

--- web-app/src/initialize.js
+++ web-app/src/initialize.js
@@ -6,6 +6,8 @@ const { wsClient } = require('./socket/client')
 const loadingView = require('./views/loading')
 
 require('./sketch')
+require('./music')
+require('./screenheight')
 
 $(document).ready(function () {
   loadingView.show()
---
+++ web-app/src/screenheight.js
@@ -0,0 +1,6 @@
+/**
+ * Created by svend on 7/11/2017.
+ */
+
+
+$('.full_screen_height').css('height',innerHeight);
