c745b48b6f242e3af20bc56fdc61501796c16416
jodusnodus <billiet.thomas@outlook.com>
2017-11-11 02:52:27 +0100

    Add delay to game starting

--- breakout-backend/src/java/nu/smashit/core/SingleplayerGame.java
+++ breakout-backend/src/java/nu/smashit/core/SingleplayerGame.java
@@ -1,33 +1,32 @@
 package nu.smashit.core;
 
 // @author Jonas
-
 import nu.smashit.socket.Client;
 import nu.smashit.core.Player.PlayerType;
 import nu.smashit.socket.actions.GameStartAction;
 
-public class SingleplayerGame extends Game{
+public class SingleplayerGame extends Game {
 
     public SingleplayerGame(String key, Client c) {
         super(key);
-        
+
         this.players = new Player[1];
         players[0] = new Player(c, PlayerType.PLAYER_1);
 
         c.setGame(this);
     }
-    
+
     @Override
     public Player getPlayer(Client c) {
         return getPlayer();
     }
-    
+
     public Player getPlayer() {
         return players[0];
     }
-    
+
     @Override
-     public void join(Client c) {
+    public void join(Client c) {
         throw new Error("Impossible to join a singleplayer game.");
     }
 
@@ -37,7 +36,7 @@ public class SingleplayerGame extends Game{
 
         this.gameLoop = new SingleplayerLoop(this);
 
-        gameLoopTimer.scheduleAtFixedRate(this.gameLoop, 0, updateInterval);
+        gameLoopTimer.scheduleAtFixedRate(this.gameLoop, startDelay, updateInterval);
     }
 
 }
