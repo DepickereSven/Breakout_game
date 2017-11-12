7dbdeec2dfd9a804d54445616446c43cb262c54e
svend <sven.depickere@student.howest.be>
2017-11-12 18:03:56 +0100

    Updated the actions
    The actions are update of singleplayer when you press retry or menu

--- web-app/src/views/singleplayer_game_loss.js
+++ web-app/src/views/singleplayer_game_loss.js
@@ -1,3 +1,7 @@
+const {
+  CreateSingleplayerRequestAction
+} = require('../actions/create_singleplayer_request')
+
 const path = 'singleplayer_game_lost.html'
 exports.path = path
 
@@ -6,8 +10,20 @@ exports.view = class PickModeView {
     this.path = path
     this.hideHeader = true
     this.viewManager = viewManager
+    this.menu = '#menu_singleplayer'
+    this.retry = '#retry_singleplayer'
+  }
+
+  handleLevelClick () {
+    window.wsClient.send(new CreateSingleplayerRequestAction())
+  }
+  sendBackToMenu (e) {
+    const data = $(e.currentTarget).data()
+    this.viewManager.go('singleplayer_menu.html', data)
   }
   onLoad () {
+    $(this.retry).on('click', this.handleLevelClick)
+    $(this.menu).on('click', this.sendBackToMenu.bind(this))
   }
 
   onUnload () {
