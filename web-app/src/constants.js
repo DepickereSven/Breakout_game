4ac758c573cb03d1cab26c299035633fc1b42717
jodusnodus <billiet.thomas@outlook.com>
2017-11-19 18:44:58 +0100

    Fix websocket URL

--- web-app/src/constants.js
+++ web-app/src/constants.js
@@ -6,7 +6,7 @@
  * API url
  * @type {string}
  */
-exports.API_URL = window.location.href.replace(/^http/, 'ws').replace(/\/$/, '') + '/socket'
+exports.API_URL = (window.location.origin + window.location.pathname).replace(/^http/, 'ws').replace(/\/$/, '') + '/socket'
 //exports.API_URL = 'ws://localhost:8080/breakout/socket'
 
 /**
