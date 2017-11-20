ce8c100b1252e02a78fe1845b77fe92160a44798
svend <sven.depickere@student.howest.be>
2017-11-20 11:23:34 +0100

    Remove logs from JS

--- web-app/src/views/login.js
+++ web-app/src/views/login.js
@@ -13,7 +13,6 @@ function getCountryCode (callback) {
 
 function handleSignIn (token) {
   window.viewManager.go('loading.html')
-  console.log(token)
   getCountryCode(country => {
     window.wsClient.send(UserLoginRequestAction.create({ token, country }))
   })
