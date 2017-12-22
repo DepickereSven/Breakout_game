d560513fd776f8e425eecf82d34ba750d1a6d262
jodusnodus <billiet.thomas@outlook.com>
2017-12-22 21:49:21 +0100

    Fix returning to original speed

--- breakout-backend/src/java/nu/smashit/core/power/StartSpeed.java
+++ breakout-backend/src/java/nu/smashit/core/power/StartSpeed.java
@@ -6,7 +6,6 @@ import nu.smashit.core.GameLoop;
 public class StartSpeed extends Power {
 
     private final int newStartSpeed;
-    private double originalDx;
     private double originalDy;
     private boolean firstTime;
 
@@ -20,18 +19,13 @@ public class StartSpeed extends Power {
     protected void doEffect(GameLoop gameLoop) {
         if (firstTime) {
             firstTime = false;
-
-            originalDx = gameLoop.getBall().getDx();
             originalDy = gameLoop.getBall().getDy();
-
-            gameLoop.getBall().setDx(newStartSpeed / (double) 3);
             gameLoop.getBall().setDy(newStartSpeed);
         }
     }
 
     @Override
     protected void undoEffect(GameLoop gameLoop) {
-        gameLoop.getBall().setDx(originalDx);
         gameLoop.getBall().setDy(originalDy);
     }
 
